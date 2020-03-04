7de6477a9ea687298defbccae7cb736d692c9b05
Johnny Ung <johnnyung91@gmail.com>
2020-03-04 00:39:07 -0800

    Able to match two cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,44 @@
-var gameCards = document.getElementById('game-cards')
+var firstCardClicked = null;
+var secondCardClicked = null;
+var firstCardClasses = null;
+var secondCardClasses = null;
 
-gameCards.addEventListener('click', handleClick)
+var gameCards = document.getElementById("game-cards");
+gameCards.addEventListener("click", handleClick);
 
 function handleClick(event) {
     if (event.target.className.indexOf("card-back") === -1) {
         return;
     }
-    console.log(event)
     var backOfCard = event.target;
-    backOfCard.className += ' hidden';
+    backOfCard.className += " hidden";
+
+    if (!firstCardClicked) {
+        firstCardClicked = event.target;
+        var firstCardSibling = firstCardClicked.previousElementSibling;
+        firstCardClasses = firstCardSibling.className;
+    } else {
+        secondCardClicked = event.target;
+        var secondCardSibling = secondCardClicked.previousElementSibling;
+        secondCardClasses = secondCardSibling.className;
+
+        gameCards.removeEventListener("click", handleClick)
+
+        if (firstCardClasses === secondCardClasses) {
+            gameCards.addEventListener("click", handleClick);
+            backToNull();
+        } else {
+            setTimeout(function() {
+                firstCardClicked.classList.remove("hidden");
+                secondCardClicked.classList.remove("hidden");
+                gameCards.addEventListener("click", handleClick);
+                backToNull();
+            }, 1500)
+        }
+    }
+}
+
+function backToNull() {
+    firstCardClicked = null;
+    secondCardClicked = null;
 }
