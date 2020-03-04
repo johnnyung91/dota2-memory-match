72032bd9912095fcbde9d48ba726c68728ad344c
Johnny Ung <johnnyung91@gmail.com>
2020-03-04 12:58:57 -0800

    Tracking Stats Feature completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ var firstCardClasses = null;
 var secondCardClasses = null;
 var matches = 0;
 var maxMatches = 9;
+var attempts = 0;
+var gamesPlayed = 0;
 
 //Global Variables end
 
@@ -33,23 +35,45 @@ function handleClick(event) {
             gameCards.addEventListener("click", handleClick);
             backToNull();
             matches++;
-            console.log("matches variable", matches);
+            attempts++;
+            displayStats();
             if (matches === maxMatches) {
                 var modal = document.querySelector(".modal-overlay");
                 modal.classList.remove("hidden");
             }
         } else {
-            setTimeout(function() {
-                firstCardClicked.classList.remove("hidden");
-                secondCardClicked.classList.remove("hidden");
-                gameCards.addEventListener("click", handleClick);
-                backToNull();
-            }, 1500);
+            setTimeout(notMatched, 1500);
         }
     }
+}
+
+function notMatched() {
+    firstCardClicked.classList.remove("hidden");
+    secondCardClicked.classList.remove("hidden");
+    gameCards.addEventListener("click", handleClick);
+    backToNull();
+    attempts++;
+    displayStats()
 }
 
 function backToNull() {
     firstCardClicked = null;
     secondCardClicked = null;
+}
+
+function displayStats() {
+    var gamesPlayedId = document.getElementById("gamesPlayed");
+    gamesPlayedId.textContent = gamesPlayed;
+
+    var attemptsId = document.getElementById("attempts");
+    attemptsId.textContent = attempts;
+
+    var accuracyId = document.getElementById("accuracy");
+    accuracyId.textContent = calculateAccuracy(attempts, matches);
+}
+
+function calculateAccuracy(attempts, matches) {
+    var percent = matches / attempts * 100;
+    var percentage = Math.round(percent * 100) / 100
+    return percentage + "%";
 }
